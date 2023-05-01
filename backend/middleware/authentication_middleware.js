50202ce88e321cb972fe42f688a96fac715588d3
ayushi9797 <soniayushi345@gmail.com>
2023-05-01 14:43:24 +0530

    updated authentication

--- backend/middleware/authentication_middleware.js
+++ backend/middleware/authentication_middleware.js
@@ -1,9 +1,11 @@
 const jwt = require("jsonwebtoken")
 require('dotenv').config()
 const authenticate = (req,res,next)=>{
-    const token = req.headers.authorization
+    const token = req.headers.authorization;
+    console.log("middlware token",token)
     if(token){
         const decoded = jwt.verify(token,"masai")
+        console.log(decoded)
         if(decoded){
             const userID = decoded.userID
             console.log(decoded)
