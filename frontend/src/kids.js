93dce8305d99e07ec8955de037a514dd74187d1a
ayushi9797 <soniayushi345@gmail.com>
2023-05-01 16:30:30 +0530

    added auth and backedn url

--- frontend/src/kids.js
+++ frontend/src/kids.js
@@ -1,21 +1,39 @@
+// login part
+
+let token = localStorage.getItem("Token");
+console.log(token);
+if (!token) {
+  window.location.href = "./signup.html";
+  alert("please login first");
+ 
+}
+
 let bag;
+
 async function fetching_data() {
   try {
-    let res = await fetch("http://localhost:4500/Kids/kid");
+    let res = await fetch("https://courageous-pike-cuff.cyclic.app/Kids/kid", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/JSON",
+        Authorization: token,
+      },
+    });
     if (res.ok) {
       let data = await res.json();
       bag = data;
       renderData(data);
     }
   } catch (error) {
-    alert(error.message);
+    console.log(error.message);
   }
 }
-fetching_data()
-let div = document.getElementById("product-cards")
+fetching_data();
+let div = document.getElementById("product-cards");
 function renderData(data) {
-  div.innerHTML = `${data.map((elem) => {
-    return ` <div id="dummy-cart">
+  div.innerHTML = `${data
+    .map((elem) => {
+      return ` <div id="dummy-cart">
         <h2>${elem.name}</h2>
         <img src="${elem.thumbnail}" alt="">
        
@@ -27,60 +45,47 @@ function renderData(data) {
         
         <button  data-id="${elem._id}" class= "btn">Add to cart</button>
 
-    </div>`
-  }).join(" ")}`
+    </div>`;
+    })
+    .join(" ")}`;
   let all_cart_btns = document.querySelectorAll(".btn");
   for (let btn of all_cart_btns) {
     btn.addEventListener("click", (e) => {
       let id = e.target.dataset.id;
-      cart(id)
-    })
+      cart(id);
+    });
   }
 }
 // let token = localStorage.getItem("token")
 let cartitems = JSON.parse(localStorage.getItem("cart-item")) || [];
 function cart(id) {
-
   let filterdata = bag.filter((elem) => {
     return elem._id == id;
   });
 
-
   let data = filterdata[0];
-  data.quantity = 1
+  data.quantity = 1;
   cartitems.push(data);
-  localStorage.setItem("cart-item", JSON.stringify(cartitems))
-  alert("Item added to cart")
-
-
-
-
+  localStorage.setItem("cart-item", JSON.stringify(cartitems));
+  alert("Item added to cart");
 }
 
 //SORT BY NAME
 
-
 // SORTING  by Price
 
-let sort = document.querySelector("#sorting-options")
-sort.addEventListener("change",sort_by_price)
+let sort = document.querySelector("#sorting-options");
+sort.addEventListener("change", sort_by_price);
 
-function sort_by_price(){
+function sort_by_price() {
   let sorting = document.querySelector("#sorting-options").value;
-  if(sorting == "0"){
-    window.location.reload()
-  }else if(sorting == "1"){
-    bag.sort((a,b)=> a.price - b.price)
-    renderData(bag)
-  }else if(sorting == "2"){
-    bag.sort((a,b)=> b.price - a.price)
-    renderData(bag)
+  if (sorting == "0") {
+    window.location.reload();
+  } else if (sorting == "1") {
+    bag.sort((a, b) => a.price - b.price);
+    renderData(bag);
+  } else if (sorting == "2") {
+    bag.sort((a, b) => b.price - a.price);
+    renderData(bag);
   }
-  
-  
 }
-
-
-
-
-
