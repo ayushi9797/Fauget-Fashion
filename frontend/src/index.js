bb64a60f45632b389bf206b6c820347963e9b801
ayushi9797 <soniayushi345@gmail.com>
2023-05-01 16:44:08 +0530

    Major updates

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,13 @@ import {footer} from "../components/footer.js"
 
 // console.log(footer())
 
+const profilename = document.querySelector("#profilename");
+let data = JSON.parse(localStorage.getItem("data"));
+let name = data.name ?? "user" 
+
+
+
+ profilename.innerText = name;
 const footerdiv=document.querySelector(".footer");
 footerdiv.innerHTML = footer()
 
